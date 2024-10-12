1dc0ae457b43d194512287d0cf3fea684eeb567b
naiara-ada <148208715+naiara-ada@users.noreply.github.com>
2024-10-12 13:01:08 +0200

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -33,11 +33,16 @@ char	*ft_strjoin(char *s1, char *s2)
 	if (!s1)
 	{
 		s1 = malloc(1);
+		if (!s1)
+			return(NULL);
 		s1[0] = '\0';
 	}
 	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (!str)
+	{
+		free(s1);
 		return (NULL);
+	}
 	while (s1[i] != '\0')
 	{
 		str[i] = s1[i];
